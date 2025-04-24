228e77b20017613a3aa4d901cda00be4712edb9c
kumarjit <kumarjeetmaity99@gmail.com>
2025-04-25 00:21:04 +0530

    redux implementtaion

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,7 +1,9 @@
-
-
-
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth, updateProfile } from "firebase/auth";
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    getAuth,
+    updateProfile,
+} from "firebase/auth";
 import { db } from "../Db/connection";
 import { doc, setDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
@@ -14,7 +16,7 @@ const authContext = createContext();
 export const useAuthContext = () => {
     const authContextState = useContext(authContext);
     return authContextState;
-}
+};
 
 // prepareing auth context
 export const AuthContextProvider = (props) => {
@@ -23,98 +25,108 @@ export const AuthContextProvider = (props) => {
 
     const authInstance = getAuth();
 
-   const handleSignUp = async (email, password, name) => {
+    const handleSignUp = async (email, password, name) => {
         try {
-            let auth = await createUserWithEmailAndPassword(authInstance, email,password);  // creating new user
+            let auth = await createUserWithEmailAndPassword(
+                authInstance,
+                email,
+                password
+            ); // creating new user
             const user = auth.user;
             // update user Display Name
             let updateUser = await updateProfile(user, {
-                displayName : name
+                displayName: name,
             });
             console.info(updateUser);
 
-             // Firebase Firestore reference
-            // const db = getFirestore();  // Get Firestore instance
 
             // Create a new document in Firestore with the user's UID as the document ID
             let userOtherInfo = await setDoc(doc(db, "users", user.uid), {
                 email: email,
                 name: name,
-                cart: { products: {}, totalPrice: 0 },   // Initially empty cart
-                orders: []  // Initially empty orders
+                cart: { products: {}, totalPrice: 0 }, // Initially empty cart
+                orders: [], // Initially empty orders
             });
 
             setLoggedUserInfo(user);
-            toast.success("Successfully signup")
+            toast.success("Successfully signup");
             return true;
         } catch (error) {
             console.log(error);
-            toast.error("Error while signup")
+            toast.error("Error while signup");
             setLoggedUserInfo(null);
             return false;
         }
-   }
+    };
 
-   const handleSignIn = async (email, password) => {
+    const handleSignIn = async (email, password) => {
         try {
-            let auth = await signInWithEmailAndPassword(authInstance, email, password);
-            setLoggedUserInfo(auth.user); 
+            let auth = await signInWithEmailAndPassword(
+                authInstance,
+                email,
+                password
+            );
+            setLoggedUserInfo(auth.user);
             console.info(auth);
-            toast.success("Successfully signin")
+            toast.success("Successfully signin");
             return true;
         } catch (error) {
-            toast.error("Error while login")
+            toast.error("Error while login");
             setLoggedUserInfo(null);
-            console.log(error)
+            console.log(error);
             return false;
         }
+    };
+    const handleLogout = () => {
+        localStorage.setItem("user", null);
+        setLoggedUserInfo(null);
+    };
 
-   }
-   const handleLogout = () => {
-    localStorage.setItem('user', null );
-    setLoggedUserInfo(null);
-   }
+    //    async function verifyIdToken(idToken) {
+    //     try {
+    //       const decodedToken = await admin.auth().verifyIdToken(idToken);
+    //       const uid = decodedToken.uid;
+    //       console.log("Decoded Token:", decodedToken);
+    //       // Now you have the UID and other claims
+    //       return decodedToken;
+    //     } catch (error) {
+    //       console.error("Error verifying ID token:", error);
+    //       throw new Error("Token is invalid or expired");
+    //     }
+    //   }
 
-//    async function verifyIdToken(idToken) {
-//     try {
-//       const decodedToken = await admin.auth().verifyIdToken(idToken);
-//       const uid = decodedToken.uid;
-//       console.log("Decoded Token:", decodedToken);
-//       // Now you have the UID and other claims
-//       return decodedToken;
-//     } catch (error) {
-//       console.error("Error verifying ID token:", error);
-//       throw new Error("Token is invalid or expired");
-//     }
-//   }
+    // updating loginuser status
+    useEffect(() => {
+        const userInfo = localStorage.getItem("user");
+        if (userInfo != "null") {
+            setLoggedUserInfo(JSON.parse(userInfo));
+        }
+    }, []);
 
-   // updating loginuser status
-   useEffect( () => {
-    const userInfo = localStorage.getItem('user');
-    if(userInfo != 'null'){
-        setLoggedUserInfo(JSON.parse(userInfo));
-    }
-   },[]);
-   
-   // updating loginuser status
-   useEffect( () => {
-    localStorage.setItem('user', loggedUserInfo != null? JSON.stringify(loggedUserInfo) : null );
-    if(loggedUserInfo == null)
-        setIsLoggedIn(false);
-    else
-        setIsLoggedIn(true);
-   },[loggedUserInfo]);
-
-   
-
+    // updating loginuser status
+    useEffect(() => {
+        localStorage.setItem(
+            "user",
+            loggedUserInfo != null ? JSON.stringify(loggedUserInfo) : null
+        );
+        if (loggedUserInfo == null) setIsLoggedIn(false);
+        else setIsLoggedIn(true);
+    }, [loggedUserInfo]);
 
     return (
-        <authContext.Provider value={{isLoggedIn,loggedUserInfo, handleSignIn, handleSignUp, handleLogout}}>
+        <authContext.Provider
+            value={{
+                isLoggedIn,
+                loggedUserInfo,
+                handleSignIn,
+                handleSignUp,
+                handleLogout,
+            }}
+        >
             {props.children}
         </authContext.Provider>
-    )
-}
-
+    );
+};
 
 // {
 //     email : "jitmaity9@gmail.com",
